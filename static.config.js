c5999f9c1bc4697e3a6814b93b0261f697b33c2d
okbrown <orlando.brown@jobsite.co.uk>
2019-01-20 00:10:28 +0000

    Refactor sitedata from config and post sorting by ID

--- static.config.js
+++ static.config.js
@@ -1,42 +1,17 @@
-import postList from './src/posts';
-const posts = postList.sort((a,b) => a.id - b.id);
+import config from 'config';
+import posts from './src/posts';
+
+const sortPosts = (a, b) => (a.id - b.id);
+const getPosts = (post) => ({ path: `/${post.slug}`, component: post.component, getData: () => ({ post }) });
+const getData = () => ({ posts });
 
 export default {
   siteRoot:'https://orlandobrown.uk',
   getSiteData: async () => ({
-    siteTitle: 'Orlando Brown - The pixel pusher blog',
-    header: {
-      logoName: 'OB',
-      mainMenuList: {
-        className: 'main-menu',
-        list: [ { name: 'Home', link: ' ' }, { name: 'About', link: 'about' } ]
-      },
-      socialMenuList: {
-        className: 'social-menu',
-        list: [
-          {
-            link: 'https://github.com/okbrown/',
-            className: 'fab fa-github-square fa-2x'
-          }, {
-            link: 'https://twitter.com/orlandobrown_uk/',
-            className: 'fab fa-twitter-square fa-2x'
-          },
-          {
-            link: 'https://www.instagram.com/orlandobrown_uk/',
-            className: 'fab fa-instagram fa-2x'
-          }
-        ]
-      }
-    },
+    ...config
   }),
   getRoutes: async () => {
-    const getData = () => ({ posts });
-    const childrenPosts = posts.map(post => ({
-      path: `/${post.slug}`,
-      component: post.component,
-      getData: () => ({ post })
-    }));
-
+    const childrenPosts = posts.sort(sortPosts).map(getPosts);
     return [
       {
         path: '/',
